3db45c280d8a39c5b3099d74bec33d2bc7450459
Kevin Avrillon <kavrillon@leadformance.com>
2018-01-24 16:06:40 +0100

    feat(): init

---
+++ assets/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-app>
+
+    <v-navigation-drawer
+      temporary
+      right
+      v-model="drawer"
+      fixed
+    >
+      <v-list>
+        <v-list-tile
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          exact
+          router
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title v-text="item.title"></v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar class="elevation-0">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+    </v-toolbar>
+
+    <v-toolbar fixed app dark class="elevation-0">
+      <v-toolbar-side-icon>
+        <v-icon large>show_chart</v-icon>
+      </v-toolbar-side-icon>
+
+      <v-toolbar-title
+        v-text="title"
+      ></v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items
+        class="hidden-xs-only"
+        v-for="(item) in items"
+        :key="item.title"
+      >
+        <v-btn exact
+               flat
+               :to="item.to"
+        >
+          <span v-text="item.title"></span>
+        </v-btn>
+      </v-toolbar-items>
+
+      <v-toolbar-side-icon
+        class="hidden-sm-and-up"
+        @click.stop="drawer = !drawer"
+      ></v-toolbar-side-icon>
+    </v-toolbar>
+
+    <v-slide-y-transition mode="out-in">
+      <router-view></router-view>
+    </v-slide-y-transition>
+  </v-app>
+</template>
+
+<script>
+  import Meta from 'mixins/meta'
+
+  export default {
+    mixins: [Meta],
+
+    data () {
+      return {
+        drawer: false,
+        items: [
+          { icon: 'show_chart', title: 'Discover', to: '/' },
+          { icon: 'person', title: 'Login', to: '/login' },
+          { icon: 'whatshot', title: 'Tests', to: '/tests' }
+        ],
+        title: 'Budg\'It'
+      }
+    }
+  }
+</script>
---
+++ pages/LoginView.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-content>
+    <v-layout column justify-center align-center>
+      <v-flex xs12 sm8>
+        <div class="text-xs-center">
+          <img src="/static/v.png" alt="Vuetify.js" class="mb-4" />
+        </div>
+        <v-card>
+          <v-card-text>
+            <v-layout row>
+              <v-flex xs4>
+                <v-subheader>Normal with hint text</v-subheader>
+              </v-flex>
+              <v-flex xs8>
+                <v-text-field
+                  name="input-1-3"
+                  label="Hint Text"
+                  single-line
+                ></v-text-field>
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-flex xs4>
+                <v-subheader>Error</v-subheader>
+              </v-flex>
+              <v-flex xs8>
+                <v-text-field
+                  name="input-3-5"
+                  label="Hint Text"
+                  value="Input text"
+                  :rules="[() => 'Username or Password is incorrect.']"
+                  error
+                  single-line
+                ></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" flat router to="/inspire">Continue</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-content>
+</template>
