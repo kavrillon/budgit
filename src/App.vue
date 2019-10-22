dcbcf66094aa2a76e6d2ded13c003aa2658d564b
Kevin Avrillon <kevin@avrillon.me>
2019-10-22 22:29:26 +0200

    feat(auth): add login/logout

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,59 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <footer>
+      <div v-if="user">User: {{ user.email }}</div>
+      <div v-if="!user" @click="openLogin">Login</div>
+      <div v-if="user" @click="openLogout">Logout</div>
+    </footer>
     <router-view />
+    <div id="netlify-modal"></div>
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+const netlifyIdentity = require('netlify-identity-widget');
+
+@Component({
+  data() {
+    return {
+      user: null
+    };
+  },
+  mounted() {
+    netlifyIdentity.init({
+      container: '#netlify-modal'
+    });
+
+    this.user = netlifyIdentity.currentUser();
+    netlifyIdentity.on('login', this.logged);
+    netlifyIdentity.on('logout', this.loggedOut);
+  },
+  methods: {
+    openLogin() {
+      netlifyIdentity.open();
+    },
+    openLogout() {
+      netlifyIdentity.open();
+    },
+    logged(user) {
+      console.log('TCL: logged -> logged');
+      this.user = user;
+      netlifyIdentity.close();
+    },
+    loggedOut() {
+      console.log('TCL: loggedOut -> loggedOut');
+      this.user = null;
+      netlifyIdentity.close();
+    }
+  }
+})
+export default class App extends Vue {}
+</script>
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,10 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <div id="netlify-modal"></div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-const netlifyIdentity = require('netlify-identity-widget');
-
-@Component({
-  components: {},
-  mounted() {
-    netlifyIdentity.init({
-      container: '#netlify-modal'
-    });
-    netlifyIdentity.open();
-  }
-})
+import { Vue } from 'vue-property-decorator';
 export default class Home extends Vue {}
 </script>
