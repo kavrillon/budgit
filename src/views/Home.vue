5d32a9d2f6675e056e58a805fe744c3f8cbd21b9
Kevin Avrillon <kevin@avrillon.me>
2019-10-26 09:13:32 +0200

    feat(auth): remove netlify auth

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,5 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <div id="netlify-modal"></div>
   </div>
 </template>
-
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-const netlifyIdentity = require('netlify-identity-widget');
-
-@Component({
-  components: {},
-  mounted() {
-    netlifyIdentity.init({
-      container: '#netlify-modal'
-    });
-    netlifyIdentity.open();
-  }
-})
-export default class Home extends Vue {}
-</script>
