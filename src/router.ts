a4ad4c95acd410c8571d1046d31e59bce88408a8
Kevin Avrillon <kevin@avrillon.me>
2019-10-29 09:51:25 +0100

    feat: add account page

--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,22 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Accounts from "./views/Accounts.vue";
+import Vue from 'vue';
+import Router from 'vue-router';
+import Accounts from './views/Accounts.vue';
 
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: Accounts
+    },
+    {
+      path: '/account/:number',
+      name: 'account',
+      component: () => import('./views/Account.vue')
     }
   ]
 });
---
+++ src/views/Account.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="account" v-if="account">
+    <h1 class="accounts__title">Account: {{ account.name }}</h1>
+    <figure class="accounts__balance">{{ account.balance }} €</figure>
+  </section>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+import { getAccount } from '@/services/account.service';
+import { Account } from '@/@types';
+
+export default Vue.extend({
+  data() {
+    return {
+      account: null as Account | null,
+      error: ''
+    };
+  },
+  async created() {
+    this.account = await getAccount(parseInt(this.$route.params.number));
+  }
+});
+</script>
