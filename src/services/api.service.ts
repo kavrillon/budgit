908e7f9110c0f2a15a28ca4f8cf098a70c77780c
Kevin Avrillon <kevin@avrillon.me>
2019-11-15 23:48:39 +0100

    refactor: lint

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,5 +1,5 @@
-export async function get(url: string) {
-  let response = await fetch(url);
-  let data = await response.json();
+export const get = async (url: string) => {
+  const response = await fetch(url);
+  const data = await response.json();
   return data;
-}
+};
