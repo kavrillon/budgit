184e822ff881523ed825a3c2257133ffa6ef61ae
Kevin Avrillon <kevin@avrillon.me>
2019-11-19 19:04:17 +0100

    refactor(board): remove history from accounts

--- import/index.ts
+++ import/index.ts
@@ -3,6 +3,7 @@ import rmfr from 'rmfr';
 
 import { importBPCE } from './bpce';
 import { Account, Board } from '@/@types';
+import { createBoardFromAccounts } from '@/services/board.service';
 
 const RESULT_FOLDER = './public/data/';
 const SOURCE_FOLDER = process.env.BUDGIT_DATA_PATH + 'bpce/';
@@ -19,28 +20,25 @@ const importAccounts = async (
   let allAccounts: Account[] = [];
 
   const bpceAccounts: Account[] = importBPCE(sourceFolder);
+
+  // Getting all accounts
   allAccounts = allAccounts.concat(bpceAccounts);
 
-  const board: Board = {
-    accounts: allAccounts,
-    id: 1,
-    total: allAccounts.reduce((total, account) => {
-      return total + account.total;
-    }, 0),
-  };
+  // Creating the default board from accounts
+  const board: Board = createBoardFromAccounts(allAccounts, 1);
 
   // Save accounts
   await cleanFolder(resultFolder);
-  saveBoard(resultFolder, board);
+  save(resultFolder, board, allAccounts);
 };
 
-const saveBoard = (folder: string, board: Board): void => {
+const save = (folder: string, board: Board, accounts: Account[]): void => {
   fs.mkdirSync(`${folder}/board`);
   fs.mkdirSync(`${folder}/accounts`);
 
   fs.writeFileSync(`${folder}/board/${board.id}.json`, JSON.stringify(board));
 
-  board.accounts.forEach((account: Account) => {
+  accounts.forEach((account: Account) => {
     fs.writeFileSync(
       `${folder}/accounts/${account.number}.json`,
       JSON.stringify(account),
--- src/services/board.service.ts
+++ src/services/board.service.ts
@@ -1,4 +1,4 @@
-import { Board } from '@/@types';
+import { Account, Board } from '@/@types';
 import { get } from '@/services/api.service';
 
 const JSON_PATH = '/data/board';
@@ -6,4 +6,20 @@ const JSON_PATH = '/data/board';
 export const getBoard = async (id: number): Promise<Board> => {
   const jsonBoard = await get(`${JSON_PATH}/${id}.json`);
   return Promise.resolve(jsonBoard);
+};
+
+export const createBoardFromAccounts = (
+  accounts: Account[],
+  id: number,
+): Board => {
+  return {
+    accounts: accounts.map((account: Account) => ({
+      ...account,
+      history: [], // No need history for board
+    })),
+    id,
+    total: accounts.reduce((total, account) => {
+      return total + account.total;
+    }, 0),
+  };
 };
