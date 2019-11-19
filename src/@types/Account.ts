4f5d9456b93de6e749d44fff6c5e7687464c82f8
Kevin Avrillon <kevin@avrillon.me>
2019-11-19 16:25:13 +0100

    refactor: rename history objects

--- src/@types/Account.ts
+++ src/@types/Account.ts
@@ -1,8 +1,8 @@
-import { YearlyHistory } from './index';
+import { YearHistory } from './index';
 
 export type Account = {
   bank: number;
-  history: YearlyHistory[];
+  history: YearHistory[];
   lastUpdate: string;
   number: number;
   name: string;
