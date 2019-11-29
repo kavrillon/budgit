76d05eb3037ffa305f1f2bb1eb99f5fed8b1a445
Kevin Avrillon <kevin@avrillon.me>
2019-11-29 21:11:37 +0100

    chore(test): unit some empty testing files

---
+++ src/services/board.service.spec.ts
@@ -0,0 +1,5 @@
+import { getBoard } from './board.service';
+
+it('should pass', () => {
+  expect(1 === 1).toBe(true);
+});
---
+++ src/services/operation.service.spec.ts
@@ -0,0 +1,5 @@
+import { getOperationsFromAccounts } from './operation.service';
+
+it('should pass', () => {
+  expect(1 === 1).toBe(true);
+});
