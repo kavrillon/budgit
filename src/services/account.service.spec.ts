81f44599087ed138eecf6582cd7cfe8e25d30a4c
Kevin Avrillon <kevin@avrillon.me>
2019-12-03 21:18:23 +0100

    chore(test): services/account

--- src/services/account.service.spec.ts
+++ src/services/account.service.spec.ts
@@ -1,5 +1,18 @@
-import { getAccounts } from './account.service';
+import { mockFetchError, mockFetchSuccess } from '../../tests/mocks/fetch';
+import { getAccount } from './account.service';
 
-it('should pass', () => {
-  expect(1 === 1).toBe(true);
+describe('getAccount', () => {
+  describe('when account exists', () => {
+    it('should return data', async () => {
+      window.fetch = mockFetchSuccess({ id: 1 });
+      getAccount(1).then(o => expect(o).toMatchObject({ id: 1 }));
+    });
+  });
+
+  describe('when no account', () => {
+    it('should reject with error', () => {
+      window.fetch = mockFetchError();
+      getAccount(1).catch(e => expect(e).toBe('No account 1'));
+    });
+  });
 });
--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,6 +1,8 @@
-export const get = async (url: string) => {
-  const response = await fetch(url);
-  if (!response.ok) throw new Error(response.statusText);
-  const data = await response.json();
-  return data;
+export const get = (url: string) => {
+  return fetch(url).then(response => {
+    if (!response.ok) return Promise.reject(response.statusText);
+
+    const data = response.json();
+    return Promise.resolve(data);
+  });
 };
